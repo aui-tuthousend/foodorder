11960ba681c183453171ed9fadb47835eb5416fb
aui_tuthousend <mraihan286@gmail.com>
2025-06-08 22:26:48 +0700

    productions

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
     if os.Getenv("ENV") != "production" {
         docs.SwaggerInfo.Host = "127.0.0.1:8080"
     } else {
-        docs.SwaggerInfo.Host = "https://foodorder-production-057c.up.railway.app" // ganti dengan domain production kamu
+        docs.SwaggerInfo.Host = "foodorder-production-057c.up.railway.app" // ganti dengan domain production kamu
     }
     
     app := fiber.New(fiber.Config{
